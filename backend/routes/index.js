599dce7dfab72ab825ed3d4c15b93f67bad23cc9
Abu Bakar <abuar9557@gmail.com>
2025-08-19 22:33:53 +0530

    authentication ho gaya

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./post");
+const post = require('./post');
+
+const localStrategy= require('passport-local');
+const passport = require('passport');
+passport.authenticate(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/profile', isLoggedIn, function(req,res){
+  res.send("Welcome to your profile page");
+});
+
+router.post('/register', function(req, res){
+  const { username, email, fullName } = req.body;
+  const newUser = new userModel({ username, email, password, fullName });
+  
+  userModel.register(newUser, req.body.password).then(function(){
+    passport.authenticate("local")(req, res, function(){
+      res.redirect('/profile')
+    })
+  })
+  
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect:'/profile',
+  failureRedirect:'/'
+}))
+
+router.post('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+
+  
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect('/')
+}
 
 module.exports = router;
---
+++ backend/routes/post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  postText: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 500 
+  },
+  likes: [{
+    type: Array, 
+    default: [], 
+  }],
+  dateCreated: {
+    type: Date,
+    default: Date.now ,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+module.exports = mongoose.model('Post', postSchema);
